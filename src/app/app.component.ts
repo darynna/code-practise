c303d2cba5f5c96aaa4c5a700e3ae251be93b6a7
Daryna Holikova <dgolikova0@gmail.com>
2024-05-28 23:46:27 +0100

    add features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,26 @@ export class AppComponent {
     new WishItem('Get coffee', true),
     new WishItem('Find grass that cuts itself', true)
   ];
+  listFilter : String = '0';
+  newWishText = '';
   title = 'code-practise';
+
+  visibleItems: WishItem[] = this.items;
+
+  addNewWish() {
+    this.items.push(new WishItem(this.newWishText));
+    this.newWishText = '';
+  }
+
+  filterChange(value : any) {
+    if (value === '0') {
+      this.visibleItems = this.items;
+    } else if (value === '1'){
+      this.visibleItems = this.items.filter((item) => !item.isComplete)
+    }else if (value === '2'){
+      this.visibleItems = this.items.filter((item) => item.isComplete)
+    }
+  }
 
   toggleItem(item: WishItem) {
     item.isComplete = !item.isComplete
